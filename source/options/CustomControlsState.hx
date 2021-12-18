66ae3ba094fccb7b80bd9fa7c9f055935e4f9a9b
Sussyboyamogus <90984822+Sussyboyamogus@users.noreply.github.com>
2021-12-18 19:59:08 +0200

    Add files via upload

--- source/options/CustomControlsState.hx
+++ source/options/CustomControlsState.hx
@@ -32,7 +32,7 @@ class CustomControlsState extends MusicBeatSubstate
 	var down_text:FlxText;
 	var left_text:FlxText;
 	var right_text:FlxText;
-	var B_text:FlxText;
+	var b_text:FlxText;
 
 	var inputvari:FlxText;
 
@@ -90,6 +90,7 @@ class CustomControlsState extends MusicBeatSubstate
 		down_text = new FlxText(200, 250, 0,"Button down x:" + _pad.buttonDown.x +" y:" + _pad.buttonDown.y, 24);
 		left_text = new FlxText(200, 300, 0,"Button left x:" + _pad.buttonLeft.x +" y:" + _pad.buttonLeft.y, 24);
 		right_text = new FlxText(200, 350, 0,"Button right x:" + _pad.buttonRight.x +" y:" + _pad.buttonRight.y, 24);
+		b_text = new FlxText(200, 40, 0,"Button b x:" + _pad.buttonb.x +" y:" + _pad.buttonb.y, 24);
 		//hitboxes
 
 		_hb = new Hitbox();
@@ -139,7 +140,7 @@ class CustomControlsState extends MusicBeatSubstate
 		add(down_text);
 		add(left_text);
 		add(right_text);
-		add(B_text);
+		add(b_text);
 
 		// change selection
 		changeSelection();
@@ -309,6 +310,7 @@ class CustomControlsState extends MusicBeatSubstate
 		down_text.text = "Button down x:" + _pad.buttonDown.x +" y:" + _pad.buttonDown.y;
 		left_text.text = "Button left x:" + _pad.buttonLeft.x +" y:" + _pad.buttonLeft.y;
 		right_text.text = "Button right x:" + _pad.buttonRight.x +" y:" + _pad.buttonRight.y;
+		b_text.text = "Button b x:" + _pad.buttonb.x +" y:" + _pad.buttonb.y;
 	}
 
 	function SpamCheck(){
