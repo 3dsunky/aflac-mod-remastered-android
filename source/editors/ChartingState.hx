1bc157be8e83b7adff052985f9c861869505feed
Sussyboyamogus <90984822+Sussyboyamogus@users.noreply.github.com>
2021-12-18 16:19:07 +0200

    Add files via upload

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -58,7 +58,10 @@ class ChartingState extends MusicBeatState
 		'Hey!',
 		'Hurt Note',
 		'GF Sing',
-		'No Animation'
+		'No Animation',
+		'Aflac Attack',
+		'Dave Note',
+		'Dave Death Note'
 	];
 	private var noteTypeIntMap:Map<Int, String> = new Map<Int, String>();
 	private var noteTypeMap:Map<String, Null<Int>> = new Map<String, Null<Int>>();
